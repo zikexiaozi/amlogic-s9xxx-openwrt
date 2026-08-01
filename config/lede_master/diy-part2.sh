f9af83057b766097447dd9afe5aa594bdb468c24
zikexiaozi <97644359@qq.com>
2026-08-02 01:04:36 +0800

    Update diy-part2.sh

--- config/lede_master/diy-part2.sh
+++ config/lede_master/diy-part2.sh
@@ -82,6 +82,34 @@ sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/g" package/base-files/files/bin/
 sed -i "s/1.openwrt.pool.ntp.org/cn.ntp.org.cn/g" package/base-files/files/bin/config_generate
 sed -i "s/2.openwrt.pool.ntp.org/cn.pool.ntp.org/g" package/base-files/files/bin/config_generate
 
+
+# 预置openclash内核
+mkdir -p files/etc/openclash/core
+# dev内核
+CLASH_DEV_URL="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-arm64.tar.gz"
+# premium内核
+CLASH_TUN_URL="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz"
+# Meta内核版本
+CLASH_META_URL="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-arm64.tar.gz"
+
+wget -qO- $CLASH_DEV_URL | gunzip -c > files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
+# 给内核权限
+chmod +x files/etc/openclash/core/clash*
+
+# meta 要GeoIP.dat 和 GeoSite.dat
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+
+# Country.mmdb
+COUNTRY_LITE_URL=https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/lite/Country.mmdb
+# COUNTRY_FULL_URL=https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb
+wget -qO- $COUNTRY_LITE_URL > files/etc/openclash/Country.mmdb
+# wget -qO- $COUNTRY_FULL_URL > files/etc/openclash/Country.mmdb
+
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
