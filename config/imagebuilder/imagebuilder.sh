611eade44c8f2c6baf53f089e864f19db6fd1478
zikexiaozi <97644359@qq.com>
2026-08-02 13:08:49 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -219,9 +219,10 @@ rebuild_firmware() {
         \
         luci-app-amlogic luci-i18n-amlogic-zh-cn \
         kmod-mt7615-common kmod-mt7615-firmware kmod-mt7615e kmod-ath kmod-ath10k kmod-ipt-physdev kmod-nf-ipvs kmod-ipt-filter kmod-nfnetlink \
-        kmod-nft-fullcone kmod-fs-btrfs kmod-fs-overlay kmod-fs-ext4 kmod-br-netfilter kmod-crypto-hash kmod-veth kmod-macvlan kmod-nf-conntrack \
+        kmod-fs-btrfs kmod-fs-ext4 kmod-br-netfilter kmod-crypto-hash kmod-veth kmod-macvlan kmod-nf-conntrack kmod-mt7601u kmod-mt76-usb \
         kmod-nf-conntrack-netlink kmod-nf-conntrack6 kmod-nf-nat kmod-nf-ipt kmod-nf-ipt6 kmod-ipt-conntrack kmod-ipt-conntrack-extra \
         kmod-ipt-conntrack-label kmod-ipt-nat kmod-ipt-nat6 kmod-ipt-extra kmod-ipt-core kmod-ipt-ipset kmod-ipt-offload kmod-nf-flow kmod-tun \
+        kmod-mt76 kmod-mt76-connac kmod-mt76-core kmod-mt7603 MAC80211_DEBUGFS MAC80211_MESH kmod-rtlwifi kmod-rtlwifi-btcoexist kmod-rtlwifi-pci kmod-rtlwifi-usb \
         \
         ${config_list} \
         "
